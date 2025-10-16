6da29fb8524867f337ff3ebf0162829b10744b7a
SOPLAY <tkdgns1341@gmail.com>
2025-10-17 00:07:40 +0900

    fix: hash else 문 제거
    - ref #11

--- internal/util/hash.go
+++ internal/util/hash.go
@@ -3,17 +3,19 @@ package util
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	if hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost); err != nil {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
 		return "", err
-	} else {
-		return string(hashed), nil
 	}
+
+	return string(hashed), nil
+
 }
 
 func CheckPasswordHash(password, hash string) (bool, error) {
 	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
 		return false, err
-	} else {
-		return true, nil
 	}
+	return true, nil
 }
