5e4820dc96c8a3b1f2e16742940316236d616550
SOPLAY <tkdgns1341@gmail.com>
2025-10-17 22:26:08 +0900

    feat: auth Service 구현
    - ref #12

---
+++ internal/service/auth_service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	"github.com/GDH-Project/auth/internal/domain"
+)
+
+type authService struct {
+	r domain.AuthRepository
+}
+
+func (svc *authService) InsertLoginLog(ctx context.Context, loginLog *domain.LoginLog) error {
+	return svc.r.InsertLoginLog(ctx, loginLog)
+}
+
+func (svc *authService) GetTokenByUserIDAndToken(ctx context.Context, token *domain.Token) (*domain.Token, error) {
+	return svc.r.GetTokenByUserIDAndToken(ctx, token)
+}
+
+func (svc *authService) InsertToken(ctx context.Context, token *domain.Token) error {
+	return svc.r.InsertToken(ctx, token)
+}
+
+func (svc *authService) DeleteTokenByUserID(ctx context.Context, userID string) error {
+	return svc.r.DeleteTokenByUserID(ctx, userID)
+}
+
+func NewAuthService(authRepo domain.AuthRepository) domain.AuthService {
+	return &authService{
+		r: authRepo,
+	}
+}
