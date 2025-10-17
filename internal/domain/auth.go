8e2d5f15e5aef1262cc90f1bc13576f00061d629
SOPLAY <tkdgns1341@gmail.com>
2025-10-17 22:15:24 +0900

    fix: auth interface 변경
      - AuthService 안토패아수 추가
      - AuthUseCase 인터페이스 변경
    
    - ref #12

--- internal/domain/auth.go
+++ internal/domain/auth.go
@@ -29,11 +29,20 @@ type Token struct {
 type AuthRepository interface {
 	InsertLoginLog(ctx context.Context, loginLog *LoginLog) error
 	InsertToken(ctx context.Context, token *Token) error
-	GetToken(ctx context.Context, token *Token) (*Token, error)
-	DeleteToken(ctx context.Context, token *Token) error
+	GetTokenByUserIDAndToken(ctx context.Context, token *Token) (*Token, error)
+	DeleteTokenByUserID(ctx context.Context, userID string) error
+}
+
+type AuthService interface {
+	InsertLoginLog(ctx context.Context, loginLog *LoginLog) error
+	GetTokenByUserIDAndToken(ctx context.Context, token *Token) (*Token, error)
+	InsertToken(ctx context.Context, token *Token) (*Token, error)
+	DeleteTokenByUserID(ctx context.Context, userID string) error
 }
 
 type AuthUseCase interface {
-	Login(ctx context.Context, user *User) (*Token, error)
-	Logout(ctx context.Context, token *Token) error
+	LoginWithPassword(ctx context.Context, email string, password string) (*Token, error)
+	Logout(ctx context.Context, accessToken string) error
+	RefreshToken(ctx context.Context, refreshToken string) (*Token, error)
+	ValidateToken(ctx context.Context, accessToken string) (*User, error)
 }
