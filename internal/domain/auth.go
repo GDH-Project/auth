e21ab3b7e174aedf9c572278ce32a2c0559b229d
SOPLAY <tkdgns1341@gmail.com>
2025-10-18 15:54:18 +0900

    feat: auth 인터페이스 변경
    - ref #12

--- internal/domain/auth.go
+++ internal/domain/auth.go
@@ -8,10 +8,10 @@ import (
 type LoginStatus string
 
 const (
-	LoginStatusSuccess       LoginStatus = "success"
-	LoginStatusPasswordError LoginStatus = "password_error"
-	LoginStatusInvalidToken  LoginStatus = "invalid_token"
-	LoginStatusRefreshToken  LoginStatus = "refresh_token"
+	LoginStatusSuccess         LoginStatus = "success"
+	LoginStatusPasswordInvalid LoginStatus = "password_invalid"
+	LoginStatusInvalidToken    LoginStatus = "invalid_token"
+	LoginStatusRefreshToken    LoginStatus = "refresh_token"
 )
 
 type LoginLog struct {
@@ -29,19 +29,19 @@ type Token struct {
 type AuthRepository interface {
 	InsertLoginLog(ctx context.Context, loginLog *LoginLog) error
 	InsertToken(ctx context.Context, token *Token) error
-	GetTokenByUserIDAndToken(ctx context.Context, token *Token) (*Token, error)
+	GetTokenByUserIDOrToken(ctx context.Context, token *Token) (*Token, error)
 	DeleteTokenByUserID(ctx context.Context, userID string) error
 }
 
 type AuthService interface {
 	InsertLoginLog(ctx context.Context, loginLog *LoginLog) error
-	GetTokenByUserIDAndToken(ctx context.Context, token *Token) (*Token, error)
+	GetTokenByUserIDOrToken(ctx context.Context, token *Token) (*Token, error)
 	InsertToken(ctx context.Context, token *Token) error
 	DeleteTokenByUserID(ctx context.Context, userID string) error
 }
 
 type AuthUseCase interface {
-	LoginWithPassword(ctx context.Context, email string, password string) (*Token, error)
+	LoginWithPassword(ctx context.Context, email string, password string, log *LoginLog) (*Token, error)
 	Logout(ctx context.Context, accessToken string) error
 	RefreshToken(ctx context.Context, refreshToken string) (*Token, error)
 	ValidateToken(ctx context.Context, accessToken string) (*User, error)
